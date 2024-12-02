b1f0e8051ee84eb13ad38a73ad8abe7ff7a6c2b5
yamlordz <misterschris@gmail.com>
2024-12-03 00:34:55 +0200

    pleir2.0

--- player.gd
+++ player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -800.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -16,7 +18,18 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = true
+	elif direction < 0:
+		animated_sprite.flip_h = false
+	
+	if velocity.y > 400:
+		animated_sprite.play("Fall")
+	else:
+		animated_sprite.play("default")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
